329ee05f5f412a9615d0e7124301d65fa46dfde0
Saurabh Kumar Suryan <saurabhsuryan99@hotmail.com>
2020-12-08 22:03:48 +0530

    nda

--- flask-api/app.py
+++ flask-api/app.py
@@ -1,41 +1,16 @@
-from flask import Flask, render_template, url_for, request, redirect, jsonify
+from flask import Flask, json, jsonify
 from flask_cors import CORS
-from predict import *
-from caption import *
+
 import os
-import warnings
-warnings.filterwarnings("ignore")
+
 
 port = int(os.environ.get("PORT", 5000))
-
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/predict-image', methods = ['POST'])
+@app.route('/')
 def upload_file():
-    if request.method == 'POST':
-        img = request.files['image']
-        img.save("static/"+img.filename)
-        caption = caption_generator("static/"+img.filename)
-        result_dic = {
-            'image' : "static/" + img.filename,
-            'description' : caption
-        }
-        result_dic['description'] = sentiment_predict(caption)
-    return jsonify({'sentiment': result_dic['description']})
-
-@app.route('/predict', methods=['GET', 'POST'])
-def predict():
-    # GET request
-    if request.method == 'GET':
-        return jsonify({"hello":"World"}) 
-    # POST request
-    if request.method == 'POST':
-        data = request.get_json()  # parse as JSON
-        user_content = data["data"]
-        result = sentiment_predict(user_content)
-        return jsonify({'sentiment': result}),200
-
+    return jsonify({"hello": "world"}), 200
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0',port=port,debug = True)
+	app.run(host='0.0.0.0',debug = True,port=port)
