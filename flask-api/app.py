22e2d37aba07bde474dee59a00e5890967c89ceb
Saurabh Kumar Suryan <saurabhsuryan99@hotmail.com>
2020-12-08 19:24:15 +0530

    And

--- flask-api/app.py
+++ flask-api/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, json, jsonify
 from flask_cors import CORS
 
+
+port = int(os.environ.get("PORT", 5000))
 app = Flask(__name__)
 CORS(app)
 
@@ -9,4 +11,4 @@ def upload_file():
     return jsonify({"hello": "world"}), 200
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0',debug = True)
+	app.run(host='0.0.0.0',debug = True,port=port)
